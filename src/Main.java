a202b85af8ac7813a9b0118885123ab48f35a1a7
Adrielram <adriel_ram@hotmail.com>
2024-09-01 22:35:42 -0300

    project created

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import PrimerEtapa.AnalizadorLexico;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AnalizadorLexico Analex = new AnalizadorLexico();
+		Analex.setMatriz(5);
+	}
+
+}
---
+++ src/PrimerEtapa/AnalizadorLexico.java
@@ -0,0 +1,13 @@
+package PrimerEtapa;
+
+public class AnalizadorLexico {
+	
+	private int MatrizAutomata[][];
+	
+	public AnalizadorLexico(){}
+	
+	public void setMatriz(int e) {
+		System.out.println(e);
+	}
+
+}
